0032ac2a416bfb7b108725b3948713dbcc2bea65
Nick Bolten <nbolten@gmail.com>
2015-10-04 23:58:13 -0700

    Added Yun's updates to pgrouting_prep

--- 4_pgrouting_prep.sql
+++ 4_pgrouting_prep.sql
@@ -1,1 +1,14 @@
--- FIXME: Why is this empty?
+DROP TABLE IF EXISTS sidewalks_ready_routing;
+
+CREATE TABLE sidewalks_ready_routing AS SELECT row_number() over() AS id,
+                                                                      q.*
+                                          FROM (   SELECT id AS o_id,
+                                                                geom,
+                                                                0 AS isCrossing
+                                                     FROM cleaned_sidewalks
+                                                UNION ALL
+                                                   SELECT id AS o_id,
+                                                                geom,
+                                                                1 as isCrossing
+                                                     FROM connection) AS q
+                                         WHERE GeometryType(q.geom) = 'LINESTRING';
