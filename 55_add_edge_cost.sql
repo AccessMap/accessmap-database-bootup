ca8a8085314d387e2741b3e4625404299fd52c27
Ubuntu <nbolten@DSSG-DB.DSSG-DB.d3.internal.cloudapp.net>
2016-01-25 06:39:06 +0000

    Added precalculation of cost function, nodes for isochrone calculations

---
+++ 55_add_edge_cost.sql
@@ -0,0 +1,5 @@
+ALTER TABLE routing
+ ADD COLUMN cost double precision ;
+
+UPDATE routing
+   SET cost = 1 * ST_length(geom) + CASE ST_length(geom) WHEN 0 THEN 0 ELSE 1e10 * POW(ABS(ele_change) / ST_length(geom), 4) END;
---
+++ 70_create_nodes_for_isochrones.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS routing_nodes;
+-- Create nodes table
+CREATE TABLE routing_nodes AS SELECT id,
+                                     ST_Centroid(ST_Collect(pt)) AS geom
+                                FROM (      (SELECT source AS id,
+                                                   ST_StartPoint(geom) AS pt
+                                              FROM routing)
+                                      UNION (SELECT target AS id,
+                                                   ST_EndPoint(geom) AS pt
+                                              FROM routing)) AS foo
+                            GROUP BY id;
