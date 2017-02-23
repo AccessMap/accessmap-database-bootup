ef9d9d890abb7789843ec099b7f77d8ca285e9c3
Nick Bolten <nbolten@gmail.com>
2017-02-22 23:38:10 -0800

    Bump to newer version of pgrouting

--- workflow.sh
+++ workflow.sh
@@ -8,13 +8,13 @@ cp -r example_city cities/seattle
 
 # Create an empty database (note: you need to kill + rm it to re-run this
 # command)
-docker run -d --name accessmapdb -p 44444:5432 -e POSTGRES_PASSWORD=test -e PGDATA=/var/lib/postgresql/data/pgdata -v $(pwd)/pgsql_data:/var/lib/postgresql/data/pgdata starefossen/pgrouting:9.4-2.1-2.1
+docker run -d --name accessmapdb -p 44444:5432 -e POSTGRES_PASSWORD=test -e PGDATA=/var/lib/postgresql/data/pgdata -v $(pwd)/pgsql_data:/var/lib/postgresql/data/pgdata starefossen/pgrouting:9.6-2.3-2.3
 
 # Download + clean the data
 docker build --tag dm . && docker run -v $(pwd):/sourcedata dm all seattle
 
 # Load the data into the database
-docker run -it --link accessmapdb -v $(pwd):/wd -w /wd starefossen/pgrouting:9.4-2.1-2.1 sh ./load.sh seattle postgres://postgres:test@accessmapdb:5432/postgres
+docker run -it --link accessmapdb -v $(pwd):/wd -w /wd starefossen/pgrouting:9.6-2.3-2.3 sh ./load.sh seattle postgres://postgres:test@accessmapdb:5432/postgres
 
 # Finish up cleaning + do pgRouting graph prep
-docker run -it --link accessmapdb -v $(pwd):/wd -w /wd starefossen/pgrouting:9.4-2.1-2.1 sh ./finalize.sh seattle postgres://postgres:test@accessmapdb:5432/postgres
+docker run -it --link accessmapdb -v $(pwd):/wd -w /wd starefossen/pgrouting:9.6-2.3-2.3 sh ./finalize.sh seattle postgres://postgres:test@accessmapdb:5432/postgres
